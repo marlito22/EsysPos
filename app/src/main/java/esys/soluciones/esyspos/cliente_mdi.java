563b1aebc5241ab2111c861401c19deb63f25d93
Net <marlito041@gmail.com>
2020-05-19 11:22:42 -0500

    Permisos

---
+++ app/src/main/java/esys/soluciones/esyspos/cliente_mdi.java
@@ -0,0 +1,14 @@
+package esys.soluciones.esyspos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cliente_mdi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cliente_mdi);
+    }
+}
