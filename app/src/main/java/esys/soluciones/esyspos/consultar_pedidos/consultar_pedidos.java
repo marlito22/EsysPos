acecebcff620f8ded027f92ab1315838fc8a9b47
marlito22 <65235690+marlito22@users.noreply.github.com>
2021-06-25 17:06:03 -0500

    consultar pedidos.

---
+++ app/src/main/java/esys/soluciones/esyspos/consultar_pedidos/consultar_pedidos.java
@@ -0,0 +1,16 @@
+package esys.soluciones.esyspos.consultar_pedidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import esys.soluciones.esyspos.R;
+
+public class consultar_pedidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultar_pedidos);
+    }
+}
